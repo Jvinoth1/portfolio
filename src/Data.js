494610e3971a2f1992e5a8ce2df149201c4cda3d
Jvinoth1 <ju.jvinoth@gmail.com>
2026-01-11 19:50:03 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,205 @@
+import {SiHtml5,SiCss3,SiTailwindcss,SiJavascript,SiReact,SiRedux,SiGithub,SiPostman,SiNodedotjs,SiExpress,SiMongodb,SiMongoose,SiNpm,SiGit,SiVercel,SiVite,SiSass,SiRender} from 'react-icons/si'
+
+export const skillData=[
+
+                {
+                    icons:SiReact,
+                    skill:"REACT",
+                    desc:"Backend",
+                    color:'#61DAFB'
+                },
+                {
+                    icons:SiNodedotjs,
+                    skill:"Node.JS",
+                    desc:"Backend",
+                    color:'#339933'
+                },
+                {
+                    icons:SiExpress,
+                    skill:"Express",
+                    desc:"Backend",
+                    color:"#fff"
+                },
+                {
+                    icons:SiMongodb,
+                    skill:"MongoDB",
+                    desc:"Database",
+                    color:"#47A248"
+                },
+                {
+                    icons:SiMongoose,
+                    skill:"Mongoose",
+                    desc:"Database",
+                    color:"#880000"
+                },
+                {
+                    icons:SiHtml5,
+                    skill:"HTML",
+                    desc:"Frontend",
+                    color:"#E34F26"
+                },
+                {
+                    icons:SiCss3,
+                    skill:"CSS",
+                    desc:"Frontend",
+                    color:"#1572B6"
+                },
+                {
+                    icons:SiTailwindcss,
+                    skill:"TailwindCSS",
+                    desc:"Frontend",
+                    color:"#1572B6"
+                },
+                {
+                    icons:SiSass,
+                    skill:"Sass",
+                    desc:"Frontend",
+                    color:"#880000"
+                },
+                {
+                    icons:SiJavascript,
+                    skill:"JavaScript",
+                    desc:"Language",
+                    color:"#F7DF1E"
+                },
+                {
+                    icons:SiRedux,
+                    skill:"REDUX",
+                    desc:"Frontend",
+                    color:"#764ABC"
+                },
+                {
+                    icons:SiGit,
+                    skill:"Git",
+                    desc:"Tool",
+                    color:"#F05032"
+                },
+                {
+                    icons:SiGithub,
+                    skill:"GitHub",
+                    desc:"Tool",
+                    color:"#fff"
+                },
+                {
+                    icons:SiNpm,
+                    skill:"NPM",
+                    desc:"Tool",
+                    color:"#F05032"
+                },
+                {
+                    icons:SiVite,
+                    skill:"VITE",
+                    desc:"Tool",
+                    color:"#646CFF"
+                },
+                {
+                    icons:SiPostman,
+                    skill:"Postman",
+                    desc:"Tool",
+                    color:"#F05032"
+                },
+                {
+                    icons:SiVercel,
+                    skill:"Vercel",
+                    desc:"Deployment",
+                    color:"#fff"
+                },
+                {
+                    icons:SiRender,
+                    skill:"Render",
+                    desc:"Deployment",
+                    color:"#fff"
+                },
+
+]
+
+export const projectData=[
+    {
+        image:"/images/bulkmail.png",
+        title:"Bulk Mail",
+        desc:"A pixel-perfect Netflix login page clone focusing on UI accuracy, responsiveness, and modern styling.",
+        skills:[" React.js","TailwindCSS", "node.js", "Express","MongoDB","Mongoose", "sendGrid", "API Ingration", "Rest APIs", "UI Development"],
+        github:"https://github.com/Jvinoth1/bulk--mail.git",
+        link:"https://bulk-mail-un7t.vercel.app/"
+    },
+    {
+        image:"/images/netflix.png",
+        title:"Netflix Login Clone",
+        desc:"A pixel-perfect Netflix login page clone focusing on UI accuracy, responsiveness, and modern styling.",
+        skills:[" React.js","TailwindCSS", "node.js"," Express","API Ingration", "Rest APIs", "UI Development"],
+        github:"https://github.com/Jvinoth1/netflix-login-page-.git",
+        link:"https://netflix-login-page-vg4f.vercel.app/"
+    },
+    {
+        image:"/images/weather.png",
+        title:"Weather Report",
+        desc:"A weather forecasting app that displays real-time weather data based on user location or searched cities.",
+        skills:["React.js", "TailwindCSS","API Ingration", "Rest APIs", "UI Development"],
+        github:"https://github.com/Jvinoth1/weather-report.git",
+        link:"https://weather-report-khaki-three.vercel.app/"
+    },
+    {
+        image:"/images/movieapp.png",
+        title:"Movie App",
+        desc:"A movie application that fetches and displays movie data with posters, ratings, and details using modern frontend technologies.",
+        skills:["HTML", "TailwindCSS", "React.js","API Ingration", "UI Development"],
+        github:"https://github.com/Jvinoth1/movie-app.git",
+        link:"https://movie-stream-app-d77x.vercel.app/"
+    },
+    {
+        image:"/images/taskmanager.png",
+        title:"Task Management",
+        desc:"A task management application to create, update, and delete daily tasks, helping users stay organized and productive.",
+        skills:["HTML", "TailwindCSS", "React.js", "jsx", "UI Development"],
+        github:"https://github.com/Jvinoth1/task-management.git",
+        link:"https://task-management-three-ruddy.vercel.app/"
+    },
+    {
+        image:"/images/apple.png",
+        title:"Apple Clone",
+        desc:"A responsive clone of Apple’s website homepage focusing on layout precision, typography, and premium UI feel.",
+        skills:["HTML", "TailwindCSS", "React.js", "jsx", "UI Development"],
+        github:"https://github.com/Jvinoth1/apple-clone.git",
+        link:"https://apple-clone-eight-eta.vercel.app/"
+    },
+    {
+        image:"/images/nostra.png",
+        title:"Nostra",
+        desc:"A modern corporate-style website showcasing services and content with a professional layout and smooth navigation.",
+        skills:["HTML", "TailwindCSS", "UI Development", "JavaScript", "DOM"],
+        github:"https://github.com/Jvinoth1/Nostra.git",
+        link:"https://jvinoth1.github.io/Nostra/"
+    },
+    {
+        image:"/images/form.png",
+        title:"Studen Registeration",
+        desc:"A CRUD-based web application to manage student records including add, edit, delete, and view student details efficiently.",
+        skills:["HTML", "TailwindCSS", "UI Development","To-do list", "JavaScript", "DOM"],
+        github:"https://github.com/Jvinoth1/registerform.git",
+        link:"https://jvinoth1.github.io/registerform/"
+    },
+    {
+        image:"/images/greenden.png",
+        title:"Greenden",
+        desc:"A responsive landing page for a plant and gardening brand, showcasing products with a clean and nature-inspired design.",
+        skills:["HTML", "TailwindCSS", "UI Development", "JavaScript"],
+        github:"https://github.com/Jvinoth1/greenden-clone.git",
+        link:"https://jvinoth1.github.io/greenden-clone/"
+    },
+    {
+        image:"/images/triadviser.png",
+        title:"TriAdvisor",
+        desc:"A travel review platform UI that allows users to explore destinations, hotels, and attractions with ratings and reviews.",
+        skills:["HTML", "CSS", "UI Development", "basic JavaScript"],
+        github:"https://github.com/Jvinoth1/Tripadvisor.git",
+        link:"https://jvinoth1.github.io/Tripadvisor/"
+    },
+    {
+        image:"/images/udemy.png",
+        title:"Udemy-Clone",
+        desc:"A responsive Udemy clone website online course platform UI with ratings",
+        skills:["HTML", "CSS", "UI Development"],
+        github:"https://github.com/Jvinoth1/udemy-clone.git",
+        link:"https://jvinoth1.github.io/udemy-clone/"
+    }
+]
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,66 @@
+function About(){
+    return(
+        <>
+         <div className="bg-black text-white py-10">
+            <div>
+
+
+                <div className="md:py-10">
+                    <h1 className="text-center text-2xl md:text-3xl font-bold text-purple-400  [text-shadow:0_4px_12px_rgba(168,85,247,0.6)]">ABOUT ME</h1>
+                </div>
+
+                <div className="md:px-48 px-2">
+                    <div>
+                        <h1 className="text-purple-300 font-bold md:text-3xl text-xl p-2 md:px-4 md:py-6">Personal Biography</h1>
+                        <p className="text-gray-300 md:text-xl md:px-4 md:py-2">I am a MERN Full Stack Developer with a strong focus on building scalable, user-centric, and high-performance web applications. I specialize in developing end-to-end solutions using React, Node.js, Express, and MongoDB, with an emphasis on clean architecture, responsive design, and maintainable code.</p>
+                        <p className="text-gray-300 md:text-xl md:px-4 py-2">Alongside full-stack development, I have hands-on experience in Prompt Engineering, where I design and optimize prompts to effectively interact with AI models, improve response accuracy, and enhance application intelligence. I understand how to integrate AI-driven features into real-world applications to boost productivity and user experience.</p>
+                        <p className="text-gray-300 md:text-xl md:px-4 md:py-2">I am a self-driven learner who stays updated with modern web technologies and best practices. I enjoy solving problems, collaborating in team environments, and continuously refining my skills to deliver reliable and impactful digital solutions. My goal is to contribute to innovative projects where technology is used to solve meaningful, real-world challenges.</p>
+                        <h1 className="text-purple-300 font-bold md:text-3xl text-2xl p-2 md:px-4 md:py-6">Education Qualification</h1>
+                        <p className="text-gray-300 md:text-xl md:px-4 md:py-2">Bachelor’s of Computer Application(BCA) – thiruvalluvar university</p>
+                        <p className="text-gray-300 md:text-xl md:px-4 py-2">Passout: 2025</p>
+                        <p></p>
+                    </div>
+
+                    {/* <div className="md:border-2 md:border-purple-600
+                    md:shadow-[0_0_25px_rgba(168,85,247,0.6)] md:rounded-xl md:p-6 text-gray-400 transition-transform duration-300 hover:scale-105">
+                       <div>
+                        <h1 className="text-center text-2xl text-purple-300 font-bold pb-2      [text-shadow:0_3px_8px_rgba(0,0,0,0.7)]">SKILLS</h1>
+                       </div>
+                        <div>
+                            <h1 className="text-purple-300 font-bold md:text-xl md:pb-2">⚡ MERN Stack Development</h1>
+                            <p>Built scalable and maintainable full-stack web applications using React, Node.js, Express, and MongoDB. Experienced in designing RESTful APIs, handling backend logic, and integrating frontend with backend efficiently.</p>
+                        </div>
+                        <div>
+                            <h1 className="text-purple-300 font-bold md:text-xl md:pb-2">🌐 Frontend Development</h1>
+                            <p>Developed responsive, accessible, and user-friendly interfaces using HTML, CSS, JavaScript, React, and Tailwind CSS. Strong focus on clean UI, component reusability, and user-centric design.</p>
+                        </div>
+                        <div>
+                            <h1 className="text-purple-300 font-bold md:text-xl md:pb-2">🔧 Backend Development</h1>
+                            <p>Hands-on experience in building RESTful APIs, implementing authentication logic, and managing server-side workflows using Node.js and Express. Focused on performance, security, and scalability.</p>
+                        </div>
+                        <div>
+                            <h1 className="text-purple-300 font-bold md:text-xl md:pb-2">🗄️ Database Management</h1>
+                            <p>Worked with MongoDB for schema design, CRUD operations, and data modeling. Basic knowledge of SQL concepts and database optimization techniques.</p>
+                        </div>
+                        <div>
+                            <h1 className="text-purple-300 font-bold md:text-xl md:pb-2">🤖 AI Integration & Prompt Engineering</h1>
+                            <p>Skilled in Prompt Engineering for designing effective prompts to interact with AI models. Experienced in optimizing prompts to improve accuracy, relevance, and productivity, and integrating AI-powered features into web applications.</p>
+                        </div>
+                        <div>
+                            <h1 className="text-purple-300 font-bold md:text-xl md:pb-2">☁️ Cloud & DevOps (Basics)</h1>
+                            <p>Familiar with deployment workflows, version control using Git & GitHub, and basic CI/CD concepts. Comfortable deploying applications to cloud platforms.</p>
+                        </div>
+                        <div>
+                            <h1 className="text-purple-300 font-bold md:text-xl md:pb-2">🚀 Performance & Code Quality</h1>
+                            <p>Focused on writing clean, maintainable code, optimizing application performance, and following best practices to deliver reliable and efficient solutions.</p>
+                        </div>
+                    </div> */}
+                </div>
+
+            </div>
+         </div>
+        </>
+    )
+}
+
+export default About
